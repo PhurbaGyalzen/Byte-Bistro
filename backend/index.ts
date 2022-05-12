0068ac840b7cf2a2e35ed50f905b8a4a7a60a7f9
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-12 17:50:51 +0000

    Fix: Setting up Database connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { initDbConnection } from './config/database'
+import passport from 'passport';
 
-dotenv.config();
+const app = express()
+const port = process.env.PORT
 
-const app: Express = express();
-const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(passport.initialize())
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+
+app.listen(port, async () => {
+  await initDbConnection()
+  
+})
+
+export { app }
