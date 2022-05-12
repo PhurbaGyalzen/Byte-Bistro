ad104700f7d800a36b7e031d0749078ff49b36ae
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-12 18:03:13 +0000

    Feat:Passport local auth success

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { initDbConnection } from './config/database'
 import passport from 'passport';
+import { default as authRoutes } from './routes/authRoutes'
 
 const app = express()
 const port = process.env.PORT
@@ -11,10 +12,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 
+app.use('/auth', authRoutes)
+
 
 app.listen(port, async () => {
   await initDbConnection()
-  
+
 })
 
 export { app }
