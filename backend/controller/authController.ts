c871819fdbc63f4b22c107eae52164ba0729eaab
Sanjib Limbu <sl.sanehang@gmail.com>
2022-05-13 09:50:32 +0545

    signin with passport backend

--- backend/controller/authController.ts
+++ backend/controller/authController.ts
@@ -3,6 +3,7 @@ import passport from 'passport'
 import { Strategy as LocalStrategy } from 'passport-local'
 
 import {initialize} from '../config/passport-config'
+import jsonwebtoken from 'jsonwebtoken'
 
 
 // initialize(passport
@@ -21,5 +22,25 @@ export const signupUser = async (
             return res.status(401).json({ message: info.message })  // 401 Unauthorized
         }
         return res.status(201).json(user)
+    })(req, res, next) //as next closure
+}
+
+export const signinUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    passport.authenticate('local-login', (err, user, info) => {
+        if (err) {
+            return next(err)
+        }
+        if (!user) {
+            return res.status(401).json({ message: info.message })  // 401 Unauthorized
+        }
+        const token = jsonwebtoken.sign({
+            id: user._id,
+            username: user.username
+        }, process.env.JWT_SECRET!, { expiresIn: '2d' })
+        return res.status(200).json({ message: 'User logged in successfully', token: token })
     })(req, res, next) //as next closure
 }
--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
-import { signupUser } from '../controller/authController'
+import { signupUser, signinUser } from '../controller/authController'
+
 
 const router = Router()
 
 
 router.post('/signup', signupUser);
+router.post(
+    '/signin',
+    signinUser,
+)
 
 
 export default router
