783ae917910d4b5ef59ecaf3d9499df53f60b112
Sunil Tamang <technicalnepal31@gmail.com>
2022-05-14 18:44:57 +0545

    feat: :sparkles: add food models

---
+++ frontend/byte_bistro/lib/Screens/home/models/food_model.dart
@@ -0,0 +1,29 @@
+class Food {
+  final String name;
+  final int price;
+  final String description;
+  final String image;
+  final bool isAvailable;
+  final int avgRating;
+  final int avgPrepTime;
+
+  Food(
+      {required this.name,
+      required this.price,
+      required this.description,
+      required this.image,
+      required this.isAvailable,
+      required this.avgRating,
+      required this.avgPrepTime});
+
+  factory Food.fromJson(Map<String, dynamic> foodData) {
+    return Food(
+        name: foodData['name'],
+        price: foodData['price'],
+        description: foodData['description'],
+        image: foodData['image'],
+        isAvailable: foodData['isAvailable'],
+        avgRating: foodData['avgRating'],
+        avgPrepTime: foodData['avgPrepTime']);
+  }
+}
