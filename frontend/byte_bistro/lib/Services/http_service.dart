c691a28571f324d3348424dc932cf050184a840d
zznixt <34276575+zznixt07@users.noreply.github.com>
2022-05-18 13:40:19 +0000

    prefer BACKEND_HOST env var to hard coded URL

--- frontend/byte_bistro/lib/Services/http_service.dart
+++ frontend/byte_bistro/lib/Services/http_service.dart
@@ -1,9 +1,14 @@
 import 'dart:io' show Platform;
 import "package:http/http.dart" as http;
 
+const PORT = 3000;
+
 class PersistentHtpp {
   static var client = http.Client();
-  // static const baseOrign = "http://192.168.101.2:3000/";
-  static const baseOrign = "http://192.168.11.64:3000/";
-  static String baseUrl = Platform.environment['EMULATOR_ORIGIN'] ?? baseOrign;
+  // static const baseHost = "192.168.101.2";
+  static const baseHost = "192.168.11.64";
+
+  static String baseUrl = 'http://' +
+      (Platform.environment['BACKEND_HOST'] ?? baseHost) +
+      ':$PORT/';
 }
