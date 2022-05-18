51a84e1d9c64a137d788b49edee01f8db54f18cc
zznixt <34276575+zznixt07@users.noreply.github.com>
2022-05-18 13:30:43 +0000

    prefer EMULATOR_ORIGIN env var to hard coded URL

--- frontend/byte_bistro/lib/Services/http_service.dart
+++ frontend/byte_bistro/lib/Services/http_service.dart
@@ -1,7 +1,9 @@
+import 'dart:io' show Platform;
 import "package:http/http.dart" as http;
 
 class PersistentHtpp {
   static var client = http.Client();
-  // static const baseUrl = "http://192.168.101.2:3000/";
-  static const baseUrl = "http://192.168.11.64:3000/";
+  // static const baseOrign = "http://192.168.101.2:3000/";
+  static const baseOrign = "http://192.168.11.64:3000/";
+  static String baseUrl = Platform.environment['EMULATOR_ORIGIN'] ?? baseOrign;
 }
