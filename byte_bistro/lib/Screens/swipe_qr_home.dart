476b288ce0d6ea3c377d836f9c636d3b7e6811b0
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-20 12:21:04 +0000

    Added Swipable Home and Qr screen

---
+++ byte_bistro/lib/Screens/swipe_qr_home.dart
@@ -0,0 +1,19 @@
+import 'package:byte_bistro/Screens/home/home.dart';
+import 'package:byte_bistro/Screens/qr_scanner.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class SwipeHome extends StatefulWidget {
+  @override
+  _SwipeHomeState createState() => _SwipeHomeState();
+}
+
+class _SwipeHomeState extends State<SwipeHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView(
+      children: [QrScannerScreen(), HomePage()],
+    ));
+  }
+}
