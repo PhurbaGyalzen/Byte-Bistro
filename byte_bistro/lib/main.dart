beedbfa6b7c79a22a086f81253b944403f535d6c
zznixt <34276575+zznixt07@users.noreply.github.com>
2022-05-21 15:23:39 +0000

    add countdown timer during food prep

--- byte_bistro/lib/main.dart
+++ byte_bistro/lib/main.dart
@@ -31,7 +31,7 @@ class ByteBistro extends StatelessWidget {
     ));
     return GetMaterialApp(
       theme: ThemeData(fontFamily: 'Roboto'),
-      initialRoute: '/admin',
+      initialRoute: '/after-order',
       debugShowCheckedModeBanner: false,
       title: 'Byte Bistro',
       getPages: [
