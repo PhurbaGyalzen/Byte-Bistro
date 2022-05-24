e2de559ef240266b92aa0553e139360d25db58b7
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-24 16:14:50 +0000

    Feat: Get all Users route

--- backend/routes/authRoutes.ts
+++ backend/routes/authRoutes.ts
@@ -1,6 +1,6 @@
-import { Router } from 'express'
+import { Router, Request, Response, NextFunction } from 'express'
 import { signupUser, signinUser } from '../controller/authController'
-
+import { User } from '@models/Users';
 
 const router = Router()
 
@@ -10,6 +10,20 @@ router.post(
     '/signin',
     signinUser,
 )
+router.get('/', async(
+    req: Request,
+	res: Response,
+	next: NextFunction
+
+) => {
+    try{
+        const users = await User.find()
+        res.status(200).json(users)
+    }
+    catch(err){
+        res.status(400).json({ message: err })
+    }
+})
 
 
 export default router
