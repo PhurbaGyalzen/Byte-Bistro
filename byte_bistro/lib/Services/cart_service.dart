9054e58fa04fb10b61c62c80d2d9599fe57be77f
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-25 04:50:01 +0000

    Get Cart route

--- byte_bistro/lib/Services/cart_service.dart
+++ byte_bistro/lib/Services/cart_service.dart
@@ -1,6 +1,34 @@
+import 'package:byte_bistro/models/cart.dart';
 import 'package:get/get.dart';
+import 'dart:convert';
 
-class CartController extends GetxController{
-  
+import 'package:byte_bistro/Services/http_service.dart';
+
+class CartService {
+  // Future<List<Cart>> getAllCart() async {
+  //   String endpoint = PersistentHtpp.baseUrl + 'cart';
+  //   try {
+  //     final response = await PersistentHtpp.client.get(Uri.parse(endpoint));
+  //     final jsonResponse = response.body;
+  //     if (response.statusCode == 200) {
+  //       return cartFromJson(jsonResponse);
+  //     } else {
+  //       return Future.error('Internal Server Error');
+  //     }
+  //   } catch (err) {
+  //     return Future.error(' Error fetching data $err');
+  //   }
+  // }
+
+  Future getSingleCart(String cartId) async {
+    String endpoint = PersistentHtpp.baseUrl + 'cart/$cartId';
+
+    final response = await PersistentHtpp.client.get(Uri.parse(endpoint));
+    final jsonResponse = response.body;
+    if (response.statusCode == 200) {
+      return cartFromJson(jsonResponse);
+    } else {
+      return null;
+    }
+  }
 }
-
---
+++ byte_bistro/lib/controller/cart_controller.dart
@@ -0,0 +1,6 @@
+import 'dart:convert';
+
+import 'package:byte_bistro/Services/http_service.dart';
+import 'package:byte_bistro/models/cart.dart';
+
+class CartController {}
