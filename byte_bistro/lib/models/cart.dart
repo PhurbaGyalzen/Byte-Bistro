29c02234c4eac4043b4ecec9e3c613156e4787bd
neeschal00 <53501296+neeschal00@users.noreply.github.com>
2022-05-25 04:22:48 +0000

    Cart: model in frontend

---
+++ byte_bistro/lib/models/cart.dart
@@ -0,0 +1,74 @@
+// To parse this JSON data, do
+//
+//     final cart = cartFromJson(jsonString);
+
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+List<Cart> cartFromJson(String str) => List<Cart>.from(json.decode(str).map((x) => Cart.fromJson(x)));
+
+String cartToJson(List<Cart> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Cart {
+    Cart({
+        @required this.id,
+        @required this.userId,
+        @required this.items,
+        @required this.tableId,
+        @required this.createdAt,
+        @required this.updatedAt,
+        @required this.v,
+    });
+
+    String id;
+    String userId;
+    List<Item> items;
+    int tableId;
+    DateTime createdAt;
+    DateTime updatedAt;
+    int v;
+
+    factory Cart.fromJson(Map<String, dynamic> json) => Cart(
+        id: json["_id"],
+        userId: json["userId"],
+        items: List<Item>.from(json["items"].map((x) => Item.fromJson(x))),
+        tableId: json["tableId"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+        v: json["__v"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "_id": id,
+        "userId": userId,
+        "items": List<dynamic>.from(items.map((x) => x.toJson())),
+        "tableId": tableId,
+        "createdAt": createdAt.toIso8601String(),
+        "updatedAt": updatedAt.toIso8601String(),
+        "__v": v,
+    };
+}
+
+class Item {
+    Item({
+        @required this.foodId,
+        @required this.qty,
+        @required this.id,
+    });
+
+    String foodId;
+    int qty;
+    String id;
+
+    factory Item.fromJson(Map<String, dynamic> json) => Item(
+        foodId: json["foodId"],
+        qty: json["qty"],
+        id: json["_id"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "foodId": foodId,
+        "qty": qty,
+        "_id": id,
+    };
+}
