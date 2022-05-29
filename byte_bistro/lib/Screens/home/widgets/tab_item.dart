3ef9b76c60edb578771dc28d446894401c33f537
Sunil Tamang <technicalnepal31@gmail.com>
2022-05-29 15:18:56 +0545

    display dynamic data in home screen

--- byte_bistro/lib/Screens/home/widgets/tab_item.dart
+++ byte_bistro/lib/Screens/home/widgets/tab_item.dart
@@ -1,3 +1,4 @@
+import 'package:byte_bistro/Screens/add_to_cart/add_to_cart.dart';
 import 'package:byte_bistro/constants/colors.dart';
 import 'package:byte_bistro/controller/food_controller.dart';
 import 'package:byte_bistro/Screens/home/models/food_model.dart';
@@ -16,8 +17,6 @@ class _TabItemState extends State<TabItem> {
   FoodController foodController = Get.find();
   @override
   Widget build(BuildContext context) {
-    final response = foodController.getAllFood();
-    print(response);
     return SizedBox(
       height: 290.0,
       child: FutureBuilder(
@@ -30,100 +29,97 @@ class _TabItemState extends State<TabItem> {
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: data.length,
-                itemBuilder: ((context, index) => GestureDetector(
-                      onTap: () {
-                        Get.toNamed('/individual');
-                      },
-                      child: Container(
-                        width: 170,
-                        margin: EdgeInsets.only(
-                          top: 60,
-                          left: 0,
-                          right: 18,
-                          bottom: 8,
-                        ),
-                        padding: EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              blurRadius: 5,
-                              offset:
-                                  Offset(0, 3), // changes position of shadow
-                              color: Color(0xFFB0CCE1).withOpacity(0.32),
+                itemBuilder: ((context, index) => Container(
+                      width: 170,
+                      margin: EdgeInsets.only(
+                        top: 60,
+                        left: 0,
+                        right: 18,
+                        bottom: 8,
+                      ),
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // changes position of shadow
+                            color: Color(0xFFB0CCE1).withOpacity(0.32),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: [
+                          Positioned(
+                              top: -59,
+                              left: 20,
+                              child: Container(
+                                width: 110,
+                                height: 110,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(80),
+                                    border: Border.all(
+                                        color: Colors.white30, width: 3)),
+                              )),
+                          Positioned(
+                            top: -80,
+                            left: 0,
+                            child: Image(
+                              image: NetworkImage(data[index].image),
+                              height: 150,
+                              width: 150,
                             ),
-                          ],
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        child: Stack(
-                          clipBehavior: Clip.none,
-                          children: [
-                            Positioned(
-                                top: -59,
-                                left: 20,
-                                child: Container(
-                                  width: 110,
-                                  height: 110,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(80),
-                                      border: Border.all(
-                                          color: Colors.white30, width: 3)),
-                                )),
-                            Positioned(
-                              top: -80,
-                              left: 0,
-                              child: Image(
-                                image: AssetImage('assets/images/foodlogo.png'),
-                                height: 150,
-                                width: 150,
-                              ),
-                            ),
-                            Positioned(
-                              top: 90,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    data[index].name,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      height: 1.5,
-                                      letterSpacing: 0.5,
-                                      color: kTextColor,
-                                    ),
+                          ),
+                          Positioned(
+                            top: 90,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  data[index].name,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    height: 1.5,
+                                    letterSpacing: 0.5,
+                                    color: kTextColor,
                                   ),
-                                  SizedBox(height: 5),
-                                  Text(
-                                    'Healthy and delicious',
-                                    style: TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w300,
-                                      height: 1.5,
-                                      color: kTextLightColor,
-                                    ),
-                                  )
-                                ],
-                              ),
+                                ),
+                                SizedBox(height: 5),
+                                Text(
+                                  data[index].description,
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w300,
+                                    height: 1.5,
+                                    color: kTextLightColor,
+                                  ),
+                                )
+                              ],
                             ),
-                            Positioned(
-                              bottom: 0,
-                              child: Row(
-                                children: const [
-                                  Text('Rs 500',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w200)),
-                                  SizedBox(width: 50),
-                                  Image(
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            child: Row(
+                              children: [
+                                Text(data[index].price.toString(),
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w200)),
+                                SizedBox(width: 50),
+                                GestureDetector(
+                                  onTap: () => Get.to(AddToCart()),
+                                  child: Image(
                                       image:
                                           AssetImage('assets/images/like.png'),
                                       height: 20,
-                                      width: 20)
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
+                                      width: 20),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
                       ),
                     )),
               ),
