7ad1fb87b486cb8edf9b8baba040cdf89561c2ba
Sunil Tamang <technicalnepal31@gmail.com>
2022-06-06 09:52:20 +0545

    feat: :sparkles: create category model

---
+++ byte_bistro/lib/models/category.dart
@@ -0,0 +1,35 @@
+// To parse this JSON data, do
+//
+//     final category = categoryFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Category> categoryFromJson(String str) =>
+    List<Category>.from(json.decode(str).map((x) => Category.fromJson(x)));
+
+String categoryToJson(List<Category> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Category {
+  Category({
+    required this.id,
+    required this.name,
+    required this.v,
+  });
+
+  final String id;
+  final String name;
+  final int v;
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+        id: json["_id"],
+        name: json["name"],
+        v: json["__v"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "_id": id,
+        "name": name,
+        "__v": v,
+      };
+}
