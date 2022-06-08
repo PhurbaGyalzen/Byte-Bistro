f2341ce5157085830e7e88002fe5b4b256a63101
Sunil Tamang <technicalnepal31@gmail.com>
2022-06-08 15:05:21 +0545

    feat: :sparkles: work on profile page

--- byte_bistro/lib/main.dart
+++ byte_bistro/lib/main.dart
@@ -87,7 +87,7 @@ class ByteBistro extends StatelessWidget {
           ),
         ),
       ),
-      initialRoute: '/adminScreen',
+      initialRoute: '/home',
       debugShowCheckedModeBanner: false,
       title: 'Byte Bistro',
       getPages: [
