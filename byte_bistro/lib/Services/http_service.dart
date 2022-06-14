67bc52352f6666348e100d70219290e7339da45d
zznixt07 <zznixt07@protonmail.com>
2022-06-14 23:23:57 +0545

    fetch token from secure storage

--- byte_bistro/lib/Services/http_service.dart
+++ byte_bistro/lib/Services/http_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import "package:http/http.dart" as http;
 import 'package:http/http.dart';
+import './storage_service.dart';
 
 const PORT = 3000;
 
@@ -13,7 +14,17 @@ class PersistentHtpp {
   static String baseUrl =
       'http://' + (dotenv.env['BACKEND_HOST'] ?? baseHost) + ':$PORT/';
 
-  static Map<String, String> headers = {};
+  static Map<String, String> headers = {
+    //   "Authorization": "Bearer " + (await Storage.get('token') ?? "")
+  };
+
+  static void setHeaders(Map<String, String> hdrs) async {
+    headers = {
+      ...headers,
+      "Authorization": "Bearer " + (await Storage.get('token') ?? "")
+    };
+  }
+
   static Future<Response> get(String path) async {
     Response response =
         await client.get(Uri.parse(baseUrl + path), headers: headers);
