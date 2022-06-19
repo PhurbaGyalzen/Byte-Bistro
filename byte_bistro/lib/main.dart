c502f4df43d23a7a429c95b728530a8d0da41104
Sanjib Limbu <sl.sanehang@gmail.com>
2022-06-19 14:57:03 +0545

    admin user icon added

--- byte_bistro/lib/main.dart
+++ byte_bistro/lib/main.dart
@@ -96,7 +96,7 @@ class ByteBistro extends StatelessWidget {
           ),
         ),
       ),
-      initialRoute: '/login',
+      initialRoute: '/adminScreen',
       debugShowCheckedModeBanner: false,
       title: 'Byte Bistro',
       getPages: [
