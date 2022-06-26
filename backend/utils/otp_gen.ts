f6c65c6bf97131772d4e443ede90c0cfd3e41aa3
zznixt07 <zznixt07@protonmail.com>
2022-06-26 11:30:48 +0545

    sort OTP by creation date before selecting

--- backend/utils/otp_gen.ts
+++ backend/utils/otp_gen.ts
@@ -27,10 +27,13 @@ export class OTP {
         await otp.save()
 
     }
+    // subtle bug. if query is not sorted and db has multiple OTPs for same email,
+	// the stale one maybe be used resulting in incorrect comparison.
     static async get(key: string): Promise<IOtp | null> {
         // return OTP.OTPs[key] || null
-        const otp = await OtpModel.findOne({email: key})
-        return otp
+        // const otp = await OtpModel.find({email: key}).sort({expireTimestamp: -1})
+        const otp = await OtpModel.find({email: key}).sort({createdAt: -1})
+        return otp[0]
     }
     static async remove(key: string): Promise<void> {
         // delete OTP.OTPs[key]
