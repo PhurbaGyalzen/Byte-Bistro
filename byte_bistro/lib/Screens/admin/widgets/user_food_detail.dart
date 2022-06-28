3597914a2b5ece6eb891043b278e254eee5282d2
Phurba Gyalzen Sherpa <sphurba861@gmail.com>
2022-06-28 15:50:58 +0545

    Add Image cache in food image detail

--- byte_bistro/lib/Screens/admin/widgets/user_food_detail.dart
+++ byte_bistro/lib/Screens/admin/widgets/user_food_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../Services/http_service.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class UserFoodDetail extends StatelessWidget {
   UserFoodDetail({Key? key}) : super(key: key);
@@ -85,13 +86,20 @@ class UserFoodDetail extends StatelessWidget {
               ),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image(
-                  image: NetworkImage(
-                    PersistentHtpp.baseUrl + data[4]['image'],
-                  ),
-                  fit: BoxFit.cover,
+                child: SizedBox(
                   width: MediaQuery.of(context).size.width,
                   height: MediaQuery.of(context).size.height * 0.3,
+                  child: CachedNetworkImage(
+                    fit: BoxFit.cover,
+                    imageUrl: PersistentHtpp.baseUrl +
+                        data[4]['image'],
+                    placeholder: (context, url) => Image(
+                        fit: BoxFit.cover,
+                        image: AssetImage(
+                          'assets/images/loading.gif',
+                        )),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                  ),
                 ),
               ),
             ),
