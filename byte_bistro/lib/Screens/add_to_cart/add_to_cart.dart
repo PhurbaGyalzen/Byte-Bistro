b115ff9d8c887c3fd7f7398ee55494749a07efcd
sunieltmg <technicalnepal31@gmail.com>
2022-06-29 13:23:32 +0545

    add OnWillPop in addToCart to disable back button

--- byte_bistro/lib/Screens/add_to_cart/add_to_cart.dart
+++ byte_bistro/lib/Screens/add_to_cart/add_to_cart.dart
@@ -18,54 +18,59 @@ class _AddToCartState extends State<AddToCart> {
   Widget build(BuildContext context) {
     final CartController cartController = Get.find();
     final cartList = Get.arguments;
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        title: Text(
-          'Cart Items',
-          style: TextStyle(fontSize: 20, letterSpacing: 1, height: 1.5),
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () => Get.offNamed('/home'),
-        ),
-        actions: [
-          Badge(
-            padding: EdgeInsets.all(6),
-            child: IconButton(
-              icon: Icon(Icons.shopping_cart_outlined),
-              onPressed: () => Get.offNamed('/home'),
-            ),
-            position: BadgePosition.topEnd(top: 5, end: 4),
-            badgeColor: Colors.black,
-            elevation: 0,
-            badgeContent: Obx(
-              () => Text(
-                cartController.noOfCartItems.value.toString(),
-                style: TextStyle(color: Colors.white),
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text(
+            'Cart Items',
+            style: TextStyle(fontSize: 20, letterSpacing: 1, height: 1.5),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () => Get.offNamed('/home'),
+          ),
+          actions: [
+            Badge(
+              padding: EdgeInsets.all(6),
+              child: IconButton(
+                icon: Icon(Icons.shopping_cart_outlined),
+                onPressed: () => Get.offNamed('/home'),
+              ),
+              position: BadgePosition.topEnd(top: 5, end: 4),
+              badgeColor: Colors.black,
+              elevation: 0,
+              badgeContent: Obx(
+                () => Text(
+                  cartController.noOfCartItems.value.toString(),
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+          backgroundColor: kPrimary,
+          foregroundColor: kTextColor,
+        ),
         backgroundColor: kPrimary,
-        foregroundColor: kTextColor,
-      ),
-      backgroundColor: kPrimary,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                CartFood(
-                  cartList: cartList,
-                ),
-                PaymentSummary(),
-              ],
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CartFood(
+                    cartList: cartList,
+                  ),
+                  PaymentSummary(),
+                ],
+              ),
             ),
           ),
         ),
