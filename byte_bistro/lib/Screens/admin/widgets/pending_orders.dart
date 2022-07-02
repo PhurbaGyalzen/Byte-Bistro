6969501f430d26d930e8f05d7cb5429c055f0da9
zznixt07 <zznixt07@protonmail.com>
2022-07-02 13:37:10 +0545

    pending orders

---
+++ byte_bistro/lib/Screens/admin/widgets/pending_orders.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PendingOrders extends StatefulWidget {
+  PendingOrders({Key? key}) : super(key: key);
+
+  @override
+  State<PendingOrders> createState() => _PendingOrdersState();
+}
+
+class _PendingOrdersState extends State<PendingOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
